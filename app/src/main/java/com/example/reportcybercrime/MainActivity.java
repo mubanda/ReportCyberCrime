2f3c3f755e0c51b8bb57140239d49b570fbc3c52
u1973964 <u1973964@unimail.hud.ac.uk>
2021-04-03 17:01:15 +0100

    Added Activities for each section as well as finished setting up the temporary activity.

--- app/src/main/java/com/example/reportcybercrime/MainActivity.java
+++ app/src/main/java/com/example/reportcybercrime/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.reportcybercrime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void displaySignInActivity(View view){
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayReportFormsActivity(View view){
+        Intent intent = new Intent(this, ReportFormsActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayMainMenuActivity(View view){
+        Intent intent = new Intent(this, MainMenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void displaySettingsActivity(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayReportListActivity(View view){
+        Intent intent = new Intent(this, ReportListActivity.class);
+        startActivity(intent);
+    }
+
+
 }
