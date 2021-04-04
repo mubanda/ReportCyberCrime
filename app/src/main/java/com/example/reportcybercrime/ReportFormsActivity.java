3eca605fd6151731823cd5615cfed98656f189fb
u1973964 <u1973964@unimail.hud.ac.uk>
2021-04-04 11:14:28 +0100

    Added to Report Forms Activity: 3 buttons each link to 3 different report form activities (finance, education, social media).

--- app/src/main/java/com/example/reportcybercrime/ReportFormsActivity.java
+++ app/src/main/java/com/example/reportcybercrime/ReportFormsActivity.java
@@ -2,7 +2,9 @@ package com.example.reportcybercrime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ReportFormsActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class ReportFormsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_forms);
+    }
+
+    public void displayFinanceActivity(View view){
+        Intent intent = new Intent(this, FinanceActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayEducationActivity(View view){
+        Intent intent = new Intent(this, EducationActivity.class);
+        startActivity(intent);
+    }
+
+    public void displaySocialMediaActivity(View view){
+        Intent intent = new Intent(this, SocialMediaActivity.class);
+        startActivity(intent);
     }
 }
